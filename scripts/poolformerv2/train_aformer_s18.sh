b2f3fbf1bddd2c339365d4b379c772882d1bed70
sakaili <383958232@qq.com>
2024-03-01 02:21:00 +0000

    Initial commit

--- scripts/poolformerv2/train_aformer_s18.sh
+++ scripts/poolformerv2/train_aformer_s18.sh
@@ -1,21 +1,21 @@
-DATA_PATH=/root/SharedData/datasets/imagenette2
+DATA_PATH=/root/workspace/datasets/CIFAR-100-dataset
 CODE_PATH=/root/workspace/Poolformer-baseline # modify code path here
 
 
-ALL_BATCH_SIZE=2048
+
+ALL_BATCH_SIZE=128
 NUM_GPU=4
-GRAD_ACCUM_STEPS=4 # Adjust according to your GPU numbers and memory size.
+GRAD_ACCUM_STEPS=16 # Adjust according to your GPU numbers and memory size.
 let BATCH_SIZE=ALL_BATCH_SIZE/NUM_GPU/GRAD_ACCUM_STEPS
 
 
 cd $CODE_PATH && sh distributed_train.sh $NUM_GPU $DATA_PATH \
---model poolformerv2_s12 --opt adamw --lr 4e-3 --warmup-epochs 5 \
+--model aformer_s18 --opt lamb --lr 8e-3 --warmup-epochs 20 \
 -b $BATCH_SIZE --grad-accum-steps $GRAD_ACCUM_STEPS \
---drop-path 0.1 \
+--drop-path 0.15 --head-dropout 0.0 \
+--log-wandb \
+--amp \
 --native-amp \
---epochs 5 \
 --pin-mem \
 --fuser nvfuser \
-
-
-
+--use-multi-epochs-loader \
